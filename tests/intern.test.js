a13d6f903f40c3ac6f9860fc3f40828742cd33fa
Jorgebelm54 <93356359+Jorgebelm54@users.noreply.github.com>
2022-03-20 16:16:52 -0500

    Add files via upload

---
+++ tests/intern.test.js
@@ -0,0 +1,19 @@
+
+const Intern = require('../lib/Intern');
+
+test ("creates an intern object", () => {
+  const intern = new Intern('Jorge', 'abcd', 'jorgebelm54@gmail.com', 'University');
+
+  expect(intern.school).toEqual(expect.any(String));
+});
+
+test ('gets the interns school', () => {
+  const intern = new Intern('Jorge', 'abcd', 'jorgebelm54@gmail.com', 'University');
+
+  expect(intern.getSchool()).toEqual(expect.any(String));
+})
+
+test ("gets intern's role", () => {
+  const intern = new Intern('Jorge', 'abcd', 'jorgebelm54@gmail.com', 'University');
+  expect(intern.getRole()).toBe('Intern')
+});
